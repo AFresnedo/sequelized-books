bf007e0ded761ce618c12328dcd39eabe6511ef7
AFresnedo <AFresnedo@users.noreply.github.com>
2018-08-17 15:10:24 -0700

    write add book form

--- controllers/books.js
+++ controllers/books.js
@@ -7,7 +7,21 @@ var router = express.Router();
 
 // define routes - remember they are relative to /books
 router.get('/', function(req, res){
-  res.render('index');
+  // define some code to run when db returns the query
+  // "then" because it's async
+  // "books" is the data returned, in this case an array of all items
+  db.book.findAll().then(function(books){
+    console.log('books returned:', books);
+    // this is INSIDE of the promise because otherwise it will render before
+    // data arrives
+    // relative to /views but not /books
+    res.render('books/index', { books: books });
+
+    // omg a then, catch!!!
+  }).catch(function(err) {
+    console.log('findAll books failed', err)
+    res.send('this is my 404, iz very nice');
+  });
 });
 
 // export router so other files can use it
